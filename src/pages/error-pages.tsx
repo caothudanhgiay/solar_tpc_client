67b01e65b6f00ceba386fdbd931705b4e5fcfff5
nguyendinhson0154 <nguyendinhson0154@gmail.com>
2026-07-19 20:34:31 +0700

    refactor the source code

--- src/pages/error-pages.tsx
+++ src/pages/error-pages.tsx
@@ -12,7 +12,7 @@ import {
 } from 'lucide-react';
 import Link from 'next/link';
 import Head from 'next/head';
-import { useTranslation } from 'next-i18next';
+import { useTranslation } from 'react-i18next';
 import { GetStaticProps } from 'next';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
